fb6d58f88fc613346c90c8d1fb29ec5e1bdef270
Him10Agr <himanshuagr.c2@gmail.com>
2024-03-07 04:02:08 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+keys = ['a', 'b', 'c']
+values = [3, 6, 1]
+
+plt.bar(keys, values)
+plt.savefig('bars.png')
+
+x = list(range(10))
+y = list(range(10))
+plt.plot(x, y)
+plt.savefig('lines.png')
